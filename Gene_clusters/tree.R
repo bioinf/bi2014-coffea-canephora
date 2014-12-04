92ad115045829dee7cf0f3599f5977bc10ea4622
drozdovapb <drozdovapb@gmail.com>
2014-12-04 21:01:20 +0300

    renewed the tree

--- Gene_clusters/tree.R
+++ Gene_clusters/tree.R
@@ -1,3 +1,5 @@
+setwd('/media/drozdovapb/big/Studies/bioinformaticsinstitute/bioinfo-lab-14/bi2014-coffea-canephora/Gene_clusters/')
+
 library(ape)
 library(phangorn)
 ?tree
@@ -23,10 +25,22 @@ plot(tr)
 #Decrease:	(28504,14834)	(18036,11517)
 
 ?text
-png('tree.png')
+png('tree.png', width=480, height=320)
 plot(tr)
 text(x=1, y=1.5, labels="(4343,407)\n(15160,32766)\n(28504,14834)", cex=.9)
 text(x=4, y=2.5, labels="(1245,3467)\n(28726,33023)\n(18036,11517)", cex=.9)
 text(x=0.5, y=2.8, labels="Expansion\nRemain\nDecrease", cex=.9)
 dev.off()
 
+plot(tr)
+text(x=1, y=1.5, labels="(4343,407)\n(15160,32766)\n(28504,14834)", cex=.9)
+text(x=4, y=2.5, labels="(1245,3467)\n(28726,33023)\n(18036,11517)", cex=.9)
+text(x=0.5, y=2.8, labels="Expansion\nRemain\nDecrease", cex=.9)
+
+png('tree.png', width=480, height=320)
+plot(tr)
+text(x=4, y=3.05, labels="4343 / 15160 / 28504", cex=.8)
+text(x=4, y=1.94, labels="3467 / 33023 / 11517", cex=.8)
+text(x=1.8, y=2.55, labels="407 / 32766 / 14834", cex=.8)
+text(x=1.8, y=1.05, labels="1245 / 28726 / 18036", cex=.8)
+dev.off()
